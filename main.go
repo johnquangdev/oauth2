6a6c7011969b5dea04ca21fd7f5de9008044e9b2
mquang2002 <mquang2022002@gmail.com>
2025-11-13 10:37:18 +0700

    feat: update application bootstrap and service layer
    - Add migration flags to main.go (--migrate-down)
    - Update cmd.go with SQL migration integration
    - Register GitHub OAuth2 handlers in delivery layer
    - Update Google OAuth2 service with proper error handling
    - Add UserInfoResp model for Google OAuth

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/johnquangdev/oauth2/cmd"
+	"github.com/johnquangdev/oauth2/cmd/sqlmigrate"
 )
 
 func main() {
+	// Define CLI flags
+	migrateDown := flag.Bool("migrate-down", false, "Run database migration rollback")
+	migrateLimit := flag.Int("limit", 1, "Number of migrations to rollback (default: 1)")
+	flag.Parse()
+
+	// Check if migrate-down flag is set
+	if *migrateDown {
+		fmt.Printf("Running migration down (limit: %d)...\n", *migrateLimit)
+		sqlmigrate.RunMigrateDown(*migrateLimit)
+		os.Exit(0)
+	}
+
+	// Run normal server
 	cmd.Run()
 }
--- service/google/google.go
+++ service/google/google.go
@@ -1,82 +1,67 @@
-package utils
+package google
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 
+	"github.com/johnquangdev/oauth2/utils"
 	configGoogle "golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/oauth2/v2"
-	"google.golang.org/api/option"
 )
 
 // struct implement interfaces 1
 type ServiceOauthGoogle struct {
 	config *configGoogle.Config
+	cfg    utils.Config
 }
 
-type Soauth2 struct {
-	googleOauth *oauth2.UserinfoService
-}
-
-// interfaces 1
-type Oauth2 interface {
-	ChangeCodeToToken(context.Context, string) (Token, error)
+// // interfaces
+type Oauth2Custom interface {
+	ChangeCodeToToken(context.Context, string) (string, error)
 	GenerateAuthURL(state string) string
+	GetUserInfoGoogle(accessToken string) (UserInfoResp, error)
 }
 
-func NewGoogleOAuthService() *ServiceOauthGoogle {
+func NewGoogleOAuthService(cfg utils.Config) (*ServiceOauthGoogle, error) {
+	// Parse scopes from config
+	scopes := []string{}
+	if cfg.Scopes_Google != "" {
+		for _, s := range strings.Split(cfg.Scopes_Google, ",") {
+			scopes = append(scopes, strings.TrimSpace(s))
+		}
+	}
+
 	config := &configGoogle.Config{
-		RedirectURL:  "http://localhost:8080/v1/auth/google/callback",
-		ClientID:     "487291772648-olmn5125kgmujjerkru6ihjh3nrbefa4.apps.googleusercontent.com",
-		ClientSecret: "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",
-		Scopes: []string{
-			"https://www.googleapis.com/auth/userinfo.email",
-			"https://www.googleapis.com/auth/userinfo.profile",
-		},
-		Endpoint: google.Endpoint,
+		RedirectURL:  cfg.RedirectUrl_Google,
+		ClientID:     cfg.ClientId_Google,
+		ClientSecret: cfg.ClientSecret_Google,
+		Scopes:       scopes,
+		Endpoint:     google.Endpoint,
 	}
 	return &ServiceOauthGoogle{
 		config: config,
-	}
-}
-
-func Newoauth2() (*Soauth2, error) {
-	service, err := oauth2.NewService(context.Background(), option.WithCredentialsFile("keyfile.json"))
-	if err != nil {
-		return &Soauth2{}, err
-	}
-	userv2 := oauth2.NewUserinfoService(service)
-	return &Soauth2{
-		googleOauth: userv2,
+		cfg:    cfg,
 	}, nil
 }
 
-func (s *ServiceOauthGoogle) GenerateAuthURL(state string) string {
-	return s.config.AuthCodeURL(state, configGoogle.AccessTypeOffline)
+func (s *ServiceOauthGoogle) GenerateAuthURL() (string, error) {
+	state, err := GenerateRandomState()
+	if err != nil {
+		return "", err
+	}
+	return s.config.AuthCodeURL(state, configGoogle.AccessTypeOffline), nil
 }
 
-func (s *ServiceOauthGoogle) ExchangeCodeForToken(ctx context.Context, code string) (OAuthResponse, error) {
-	token, err := s.config.Exchange(ctx, code)
+func (s *ServiceOauthGoogle) GetUserInfoGoogle(accessToken string) (*UserInfoResp, error) {
+	req, err := http.NewRequest(http.MethodGet, "https://www.googleapis.com/oauth2/v2/userinfo", nil)
 	if err != nil {
-		return OAuthResponse{}, err
-	}
-
-	response := OAuthResponse{
-		AccessToken:  token.AccessToken,
-		TokenType:    token.TokenType,
-		RefreshToken: token.RefreshToken,
-		ExpiresIn:    token.Expiry.String(),
-	}
-	return response, nil
-}
-
-func (s Soauth2) GetUserInfoGoogle(accessToken string) (UserResponse, error) {
-	req, err := http.NewRequest("GET", "https://www.googleapis.com/oauth2/v2/userinfo", nil)
-	if err != nil {
-		return UserResponse{}, err
+		return nil, fmt.Errorf("create request: %w", err)
 	}
 
 	req.Header.Set("Authorization", "Bearer "+accessToken)
@@ -84,32 +69,40 @@ func (s Soauth2) GetUserInfoGoogle(accessToken string) (UserResponse, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return UserResponse{}, err
+		return nil, fmt.Errorf("send request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return UserResponse{}, fmt.Errorf("google API error: %s", resp.Status)
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("google API error (%d): %s", resp.StatusCode, string(bodyBytes))
 	}
 
-	var userInfo UserResponse
-	err = json.NewDecoder(resp.Body).Decode(&userInfo)
-	if err != nil {
-		return UserResponse{}, err
+	var UserInfo UserInfoResp
+	if err := json.NewDecoder(resp.Body).Decode(&UserInfo); err != nil {
+		return nil, fmt.Errorf("decode user info: %w", err)
 	}
-
-	return userInfo, nil
+	UserInfo.Provider = "google"
+	return &UserInfo, nil
 }
 
-//test sau nhé------
-// func (s Soauth2) LGetUserInfo(accessToken string) (UserResponse, error) {
-// 	userinfo, err := s.googleOauth.Get().Do(googleapi.QueryParameter("Authorization", "Bearer "+accessToken))
-// 	if err != nil {
-// 		return UserResponse{}, err
-// 	}
-// 	return UserResponse{
-// 		Email:   userinfo.Email,
-// 		Name:    userinfo.GivenName,
-// 		Picture: userinfo.Picture,
-// 	}, nil
-// }
+func (o ServiceOauthGoogle) ChangeCodeToToken(ctx context.Context, code string) (string, string, error) {
+	token, err := o.config.Exchange(ctx, code)
+	if err != nil {
+		return "", "", fmt.Errorf("exchange code failed by err: %w", err)
+	}
+	if !token.Valid() {
+		return "", "", fmt.Errorf("invalid token received from Google")
+	}
+	idToken := token.Extra("id_token").(string)
+	return token.AccessToken, idToken, nil
+}
+
+func GenerateRandomState() (string, error) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
